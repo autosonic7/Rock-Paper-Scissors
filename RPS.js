f431c9a67c7b2303a51aac2b48dd302684e72067
Anthony <autosonic7@hotmail.com>
2022-10-13 23:31:14 -0700

    Computer Choice function is set

---
+++ RPS.js
@@ -0,0 +1,12 @@
+function getComputerChoice()
+{
+    let options = ["Rock", "Paper", "Scissors"];
+    let choice = options[Math.floor(Math.random()*options.length)];
+    // console.log(choice);
+    
+}
+getComputerChoice();
+// function playRound(playerSelection, computerSelection)
+// {
+    
+// }
