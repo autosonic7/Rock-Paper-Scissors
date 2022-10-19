a558a34a5544b08b37ad778babc49f1578b2161f
Anthony <autosonic7@hotmail.com>
2022-10-19 00:00:28 -0700

    implemented a score system in console

--- RPS.js
+++ RPS.js
@@ -2,11 +2,13 @@ function getComputerChoice()
 {
     let options = ["rock", "paper", "scissors"];
     let choice = options[Math.floor(Math.random()*options.length)];
-     //   console.log(choice);
+     //    console.log(choice);
       return choice;
     
 }
 // let playerSelection = prompt("Please enter Rock, Paper, or Scissors: ");
+let score = 0;
+let computerscore = 0;
 
 function playRound(playerSelection, computerSelection)
 {
@@ -19,31 +21,37 @@ function playRound(playerSelection, computerSelection)
     else if (playerSelection.toLowerCase() ==="rock" && computerSelection ==="scissors")
     {
          alert("You win! Rock beats scissors!");
+         score++;
     }
 
     else if (playerSelection.toLowerCase() ==="paper" && computerSelection ==="rock")
     {
          alert("You win! Paper beats rock!");    
+         score++;
     }
 
     else if (playerSelection.toLowerCase() ==="scissors" && computerSelection ==="paper")
     {
-         alert("You win! Scissors beats paper!");     
+         alert("You win! Scissors beats paper!");   
+         score++;  
     }
 
     else if (playerSelection.toLowerCase() ==="scissors" && computerSelection ==="rock")
     {
          alert("You lose! Rock beats scissors!");
+         computerscore++;
     }
 
     else if (playerSelection.toLowerCase() ==="rock" && computerSelection ==="paper")
     {
          alert("You lose! Paper beats rock!");
+         computerscore++;
     }
 
     else if (playerSelection.toLowerCase() ==="paper" && computerSelection ==="scissors")
     {
          alert("You lose! Scissors beats paper!");
+         computerscore++;
     }
 
     else
@@ -58,10 +66,21 @@ function game()
     {
      let playerSelection = prompt("Please enter Rock, Paper, or Scissors: ");
      let computerSelection = getComputerChoice();
-      playRound(playerSelection, computerSelection);
+     playRound(playerSelection, computerSelection);
+     console.log("You:"+score);
+     console.log("Computer:"+computerscore);
     }
+    
+    if(score > computerscore)
+     {
+          alert("You won!")
+     }
+     else if (score < computerscore)
+     {
+            alert("The computer wins!") 
+     } 
 
 }
-// game();
+ game();
 //  let playerSelection = "rock";
 
